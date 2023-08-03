063c28a812098d5f3fdb0295da8547d65cb577eb
Yasfi <yasfi.mail@gmail.com>
2023-08-03 11:57:52 +0700

    fix

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module RubyRailsPostgres
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.hosts << "rails-production-9829.up.railway.app"
   end
 end
